04aa87627f21322cd7941ee550ca72ab12efc3b2
wxk <1037126702@qq.com>
2020-11-19 16:49:21 +0800

    测试

---
+++ src/main/java/com/fh/contorller/TestController.java
@@ -0,0 +1,14 @@
+package com.fh.contorller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("test")
+public class TestController {
+    @RequestMapping("demo")
+    public String demo(){
+        return "success";
+    }
+}
